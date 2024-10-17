02584dc596b7c7fff20913ba5d10fa75607454ce
Leo Bastigkeit <leo.bastigkeit@student.htw-berlin.de>
2024-10-17 10:42:49 +0200

    Refactoring with Class OptionsMenu

--- ProjectEditor/main.cpp
+++ ProjectEditor/main.cpp
@@ -1,29 +1,30 @@
+#include "OptionsMenu.h"
+#include <functional> // For std::bind
 #include "Editor.h"
 #include "DsaDebug.h"
 
 
 int main(int argc, char* argv[]) {
-    Editor Editor;
+    // Redirect std::cerr to a file
+    freopen("error_log.txt", "w", stderr);
 
-    //Options menue
-    Platform::clearScreen();
-    int opt = 0;
-    std::string options = "Load Level\nCreate Renderobject\nCreate Moveruleset\nQuit";
-    while (opt != std::ranges::count(options, '\n')) {
-        opt = DsaDebug::menueScreen(options, opt, "OPTIONS:\n----------------------\n", "\n----------------------");
-        Platform::clearScreen();
+    // Editor
+    Editor editor;
 
-        switch (opt) {
-        case 0:
-            Editor.LoadLevel();
-            break;
-        case 1:
-            Editor.CreateRenderobject();
-            break;
-        case 2:
-            Editor.CreateMoveRuleSet();
-            break;
-        }
-        
-    }
+	// Create options menu
+	OptionsMenu optM;
+	optM.setOption(0);	//Start option
+	// Attach functions using std::bind to bind the member functions to the object
+	optM.attachFunction(std::bind(&Editor::LoadLevel, &editor), "Load Level");
+	optM.attachFunction(std::bind(&Editor::CreateRenderobject, &editor), "Create Renderobject");
+    optM.attachFunction(std::bind(&Editor::CreateMoveRuleSet, &editor), "Create Moveruleset");
+	
+	optM.render();		//First render for display
+	
+	//Loop until exit is chosen
+	volatile int status = 0;
+	while(status != OptionsMenu::statusExit){status = optM.update();}
+    
+	return 0;
+
 }
