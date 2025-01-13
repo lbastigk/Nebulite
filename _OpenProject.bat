1821d52a53edbe269d1f1e6a30de034bed24c79b
Leo Bastigkeit <leo.bastigkeit@student.htw-berlin.de>
2025-01-13 12:54:46 +0100

    removed some unnecessary opening scripts, WIP of install script

--- _OpenProject.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Navigate to the directory where this script is located
-cd /d "%~dp0"
-
-rem Open the project in VS Code
-code .
--- _OpenProject.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Navigate to the directory where this script is located
-cd "$(dirname "$0")" || exit
-
-# Open the project in VS Code
-code .
-
