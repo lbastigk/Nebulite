e16008ceaa35026d5bcd3b21918c6fcc850c0554
lbastigk <leo.bastigkeit@gmail.com>
2025-05-30 17:15:17 +0200

    Better formatting of Benchmark cli output

--- Application/Benchmark.sh
+++ Application/Benchmark.sh
@@ -3,15 +3,21 @@
 runs=10           # Number of runs to average
 total=0
 
+echo ""
+echo "----------------------------------------"
+echo "Benchmarking Nebulite with $runs runs..."
+echo ""
+echo ""
 for ((i=1; i<=runs; i++)); do
     start=$(date +%s.%N)
-    ./bin/Nebulite task TaskFiles/benchmark_gravity.txt
+    ./bin/Nebulite task TaskFiles/benchmark_gravity.txt >> /dev/null
     end=$(date +%s.%N)
 
     runtime=$(echo "$end - $start" | bc -l)
-    echo "Run #$i: $runtime s"
+    echo " - Run #$i: $runtime s"
     total=$(echo "$total + $runtime" | bc -l)
 done
 
 average=$(echo "$total / $runs" | bc -l)
-echo "Average runtime over $runs runs: $average s"
+echo ""
+echo "[RESULT] Average runtime over $runs runs: $average s"
