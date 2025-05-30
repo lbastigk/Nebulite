6c312132299dee713ad463c6f390525b6a824213
lbastigk <leo.bastigkeit@gmail.com>
2025-05-30 15:05:03 +0200

    Benchmark.sh is now an actual subcomponent of Tests.sh

--- Application/Tests.sh
+++ Application/Tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #./bin/Nebulite task TaskFiles/crash_test.txt
 ./bin/Nebulite task TaskFiles/gravity_classic.txt
-./bin/Nebulite task TaskFiles/benchmark_gravity.txt
+./Benchmark.sh
 ./bin/Nebulite task TaskFiles/obj_oob.txt
