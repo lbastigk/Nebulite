6d709083a208ad922c44f3c36438984fdc6728ab
lbastigk <leo.bastigkeit@gmail.com>
2025-08-08 13:38:12 +0200

    Integrated new tests for JSONTree

--- Application/Tests.sh
+++ Application/Tests.sh
@@ -45,6 +45,8 @@ tests=(
     "task TaskFiles/Tests/Short/echo_global_value.txt"
     "task TaskFiles/Tests/Short/print_time.txt"
     "task TaskFiles/Tests/Short/init_print_global.txt"
+    "task TaskFiles/Tests/Short/push_pop.txt"                          # array push/pop functionality of JSONTree
+    "task TaskFiles/Tests/Short/json_retrieval.txt"                    # JSON retrieval functionality of JSONTree
     "task TaskFiles/Tests/Short/Object_spawn_and_state_printing.txt"
     "task TaskFiles/Tests/Short/Object_out_of_bounds.txt"              # Check if Renderer correctly handles objects going out of bounds
     "task TaskFiles/Tests/Short/gravity_classic_some_frames.txt"       # Classic Solar system with 2 Planets and 1 Sun
