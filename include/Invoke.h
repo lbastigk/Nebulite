8be812f61885a5b088b1e6b94bc30c7c8edf25d3
lbastigk <leo.bastigkeit@gmail.com>
2025-08-08 18:10:41 +0200

    Some ideas for a more versatile invoke system

--- include/Invoke.h
+++ include/Invoke.h
@@ -212,8 +212,33 @@ public:
       "functioncalls_other": []   // vector of function calls, e.g. "add_invoke ./Resources/Invokes/gravity.json"
      }
     */
+    // TODO: Idea for Invoke ruleset overwrites:
+    // In addition, add the field "overwrites" to the JSON doc:
+    // "overwrites": {
+    //     "key1": "value1",
+    //     "key2": "value2"
+    // }
+    // Then, on parsing, the overwrites are applied:
+    // $(overwrites.key1) would be replaced by "value1"
+    // If, however, an overwrite is not found:
+    // $(overwrites.key3) would be replaced by $(global.key3)
+    // This allows us to flexibly overwrite values in the invoke without changing the original JSON file.
+    // Also, the behavior is well-defined, as it defaults to the global value if no overwrite is defined.
+    // Note: retrieval of overwrites in a type object might be difficult. Instead, perhaps:
+    // myInvoke.json|push-back overwrites 'key1 -> value1'
+    // Example JSON:
+    /*
+    "overwrites": {
+        "key1 -> value1",
+        "key2 -> value2"
+    }
+    */
+    // This makes subkey-overwrites easier to parse, e.g.: "overwrites" [ "physics.G -> 9.81" ] 
+    // turns an "$(overwrites.physics.G)" into "9.81" and 
+    // defaults to "$(global.physics.G)" if not overwritten.
     
-    // Parsed into expressions...:
+    //---------------------------------------------
+    // Invoke epxressions are parsed into specific structs:
     struct AssignmentExpression{
       enum class Operation {null, set,add,multiply,concat};
       Operation operation;                      // set, add, multiply, concat
@@ -223,6 +248,7 @@ public:
       std::string value;                        // e.g. "0", "$($(self.posX) + 1)"
       bool valueContainsReference = true;       // if value contains a reference keyword, e.g. "$(self.posX)" or "$(global.time.t)"
     };
+    //---------------------------------------------
     // ... Inside Entries:
     struct Entry{
       std::string topic = "all";                                  // e.g. "gravity", "hitbox", "collision"
