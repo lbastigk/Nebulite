26601cc08bf8662a5236446c7c3447758b238f11
lbastigk <leo.bastigkeit@gmail.com>
2025-08-18 14:09:48 +0200

    Proper documentation for GTE_Debug.h

--- include/GTE_Debug.h
+++ include/GTE_Debug.h
@@ -1,3 +1,9 @@
+/**
+ * @file GTE_Debug.h
+ * 
+ * This file contains an expansion of the GlobalSpaceTree for debugging capabilities.
+ */
+
 #pragma once
 
 #include "ErrorTypes.h"
@@ -6,6 +12,13 @@
 namespace Nebulite {
 class GlobalSpace; // Forward declaration of domain class GlobalSpace 
 namespace GlobalSpaceTreeExpansion {
+
+/**
+ * @brief Debugging utilities for the GlobalSpaceTree.
+ * 
+ * This class binds various debugging functions to the GlobalSpaceTree
+ * and holds the utilities to redirect error output.
+ */
 class Debug : public Nebulite::FuncTreeExpansion::Wrapper<Nebulite::GlobalSpace, Debug> {
 public:
     using Wrapper<Nebulite::GlobalSpace, Debug>::Wrapper; // Templated constructor from Wrapper, call this->setupBindings()
@@ -14,38 +27,97 @@ public:
 
     //--------------------------------------------------------
     // TEST: Binding a dummy function with a name that already exists in the subtree
+    // This function exists just for testing purposes and is not meant to be used in production code.
+    // Instead, it serves as a placeholder to demonstrate the binding mechanisms collision detection
+    // Uncomment the bind in setupBindings() to test the collision detection.
+    // The binary compilation will work, but execution will fail.
     Nebulite::ERROR_TYPE set(int argc, char* argv[]) {
-        // Binding a function with the name "set" is not allowed as it already exists in the JSONTree
+        // Binding a function with the name "set" is not allowed as it already exists in the subtree JSONTree
         return Nebulite::ERROR_TYPE::NONE;
     }
 
     //----------------------------------------
     // Available Functions
 
-    // Error log activation/deactivation
+    /**
+     * @brief Activates or deactivates error logging to a file
+     * 
+     * This function toggles the error logging status between the standard error output
+     * and a specified log file.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: inputs are "on" or "off"
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     */
     Nebulite::ERROR_TYPE errorlog(int argc, char* argv[]);
 
-    // Print global doc to cout
+    /**
+     * @brief Prints the global document to the console.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: no inputs available
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     */
     Nebulite::ERROR_TYPE printGlobal(int argc, char* argv[]);
 
-    // Print state to cout
+    /**
+     * @brief Prints the current state of the renderer to the console.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: no inputs available
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     */
     Nebulite::ERROR_TYPE printState(int argc, char* argv[]);
 
-    // Log global doc to file
+    /**
+     * @brief Logs the global document to a file.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: inputs are <filenames>. Logs to all provided files.
+     * Default is "global.log.jsonc" if no name was provided
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     */
     Nebulite::ERROR_TYPE logGlobal(int argc, char* argv[]);
 
-    // Log state to file
+    /**
+     * @brief Logs the current state of the renderer to a file.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: inputs are <filenames>. Logs to all provided files.
+     * Default is "state.log.jsonc" if no name was provided
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     */
     Nebulite::ERROR_TYPE logState(int argc, char* argv[]);
 
-    // Attaches functioncall that is executed on each tick
-    // TODO: move to GTE_General
+    /**
+     * @brief Attach a command to the always-taskqueue that is executed on each tick.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: inputs are <command>. The command to attach.
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     * 
+     * @todo Move to GTE_General
+     */
     Nebulite::ERROR_TYPE always(int argc, char* argv[]);
 
-    // Clears all always-functioncalls
-    // TODO: move to GTE_General
+    /**
+     * @brief Clears the entire always-taskqueue.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: no inputs available
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     * 
+     * @todo Move to GTE_General
+     */
     Nebulite::ERROR_TYPE alwaysClear(int argc, char* argv[]);
 
-    // [DEBUG] Get and store a standard renderobject for reference to ./Resources/Renderobjects/standard.jsonc
+    /**
+     * @brief Logs a standard render object to a file: ./Resources/Renderobjects/standard.jsonc.
+     * 
+     * @param argc The argument count
+     * @param argv The argument vector: no inputs available
+     * @return Nebulite::ERROR_TYPE Potential errors that occured on command execution
+     */
     Nebulite::ERROR_TYPE render_object(int argc, char** argv);
 
 
