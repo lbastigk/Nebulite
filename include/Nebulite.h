a965d614797f2e0e59ea30a20c76637a23915c58
lbastigk <leo.bastigkeit@gmail.com>
2025-08-25 13:03:44 +0200

    Ideas for new Structure

--- include/Nebulite.h
+++ include/Nebulite.h
@@ -16,11 +16,28 @@
  * @brief Contains all classes, functions, types and variables related to the Nebulite framework.
  */
 namespace Nebulite{
+    //-----------------------
+    // TODOS
+
+    namespace Core{}        // Renderer, RenderObject, Environment etc.
+
+    namespace Constants{}   // keyNames, ThreadSettings, ErrorTypes
+
+    namespace Utility{}     // JSON, StringHandler, Time, VirtualDouble, Expression
+
+    namespace Expansion{}   // All FuncTreeExpansions: Nebulite::Expansion::GlobalSpace::..., Nebulite::Expansion::RenderObject::..., Nebulite::Expansion::JSON::...
+
+    // Perhaps some more structure if needed
+    // Utility could get cluttered this way
+
+    //-----------------------
 
     /**
      * @namespace Nebulite::FuncTreeExpansion
      * @brief Contains all classes, functions, types and variables related to the FuncTree expansions.
      * See files `GTE_*.h`, `RTE_*.h` and `JTE_*.h` for usage.
+     *
+     * @todo this is only used for the wrapper class. move to utility: Nebulite::Utility::ExpansionWrapper
      */
     namespace FuncTreeExpansion{}
 
