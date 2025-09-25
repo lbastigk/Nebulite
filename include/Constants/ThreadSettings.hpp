2fb99351229a20bf19f32b6572641d6d994b2765
Leo <leo.bastigkeit@gmail.com>
2025-09-25 21:55:47 +0200

    Update benchmark results in ThreadSettings.hpp for SDL2 build and OS switch

--- include/Constants/ThreadSettings.hpp
+++ include/Constants/ThreadSettings.hpp
@@ -38,4 +38,5 @@ Date            Result      THREADED_MIN_BATCHSIZE  BATCH_COST_GOAL INVOKE_EXPR_
 2025-08-14:     14.4s       500                     500             10
 2025-08-28:     14.2s       500                     500             10                      # smaller tweaks perhaps, or just general os fluctuations
 2025-09-03:     10.2s       500                     500             10                      # Fixed flushing issue, improved cache handling, less frequent flushes
+2025-09-25:      9.6s       500                     500             10                      # New SDL2 build + switch from Ubuntu 25.04 to Fedora 42
 */
