e1397b0778fb6f7e4a82b38632c5d71615f358c5
Leo <leo.bastigkeit@gmail.com>
2025-10-10 13:52:17 +0200

    Remove MemLeakTest.sh script as it is no longer needed

--- Scripts/MemLeakTest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -rf massif_output.out ; ulimit -n 32768 && valgrind --tool=massif --massif-out-file=massif_output.out ./bin/Nebulite_Debug task TaskFiles/Tests/vscode_debug.nebs && massif-visualizer massif_output.out
