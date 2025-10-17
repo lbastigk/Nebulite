974c4de405069383e9f0eb04fe194c5628ddbc2e
Leo <leo.bastigkeit@gmail.com>
2025-10-18 00:40:21 +0200

    Reorder member initialization in GlobalSpace constructor for consistency

--- src/Core/GlobalSpace.cpp
+++ src/Core/GlobalSpace.cpp
@@ -7,9 +7,9 @@
 Nebulite::Core::GlobalSpace::GlobalSpace(const std::string binName)
 : Nebulite::Interaction::Execution::Domain<Nebulite::Core::GlobalSpace>("Nebulite", this, &global, this),
   global(this),                       // Link the global document to the GlobalSpace
-  docCache(this),                     // Init with reference to GlobalSpace
   renderer(this, &cmdVars.headless),  // Renderer with reference to GlobalSpace and headless mode boolean
-  invoke(this)                        // Invoke with reference to GlobalSpace
+  invoke(this),                       // Invoke with reference to GlobalSpace
+  docCache(this)                      // Init with reference to GlobalSpace
 {
     //------------------------------------------
     // Setup tasks                         
