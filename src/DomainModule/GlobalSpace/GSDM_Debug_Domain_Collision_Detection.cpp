1563ec9c2167f9f6da8d4d75ad975b66829de842
Leo <leo.bastigkeit@gmail.com>
2025-10-19 19:30:32 +0200

    Enhance collision detection functions to accept mode arguments for success or failure scenarios

--- src/DomainModule/GlobalSpace/GSDM_Debug_Domain_Collision_Detection.cpp
+++ src/DomainModule/GlobalSpace/GSDM_Debug_Domain_Collision_Detection.cpp
@@ -19,8 +19,28 @@ const std::string Debug_Domain_Collision_Detection::collisionDetect_desc = "Util
 // Available Functions
 
 Nebulite::Constants::Error Debug_Domain_Collision_Detection::debug_collisionDetect_function([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-    // This will fail, as the function name is already registered in GlobalSpace
-    bindFunction(&Debug_Domain_Collision_Detection::debug_collisionDetect_function, debug_collisionDetect_function_name, &debug_collisionDetect_function_desc);
+    bool fail = true;
+    if(argc >= 2){
+        std::string mode = argv[1];
+        if(mode == "succeed"){
+            fail = false;
+        }
+        else if(mode == "fail"){
+            fail = true;
+        }
+        else{
+            return Nebulite::Constants::ErrorTable::FUNCTIONAL::UNKNOWN_ARG();
+        }
+    }
+
+    if(fail){
+        // This will fail, as the function name is already registered in GlobalSpace
+        bindFunction(&Debug_Domain_Collision_Detection::debug_collisionDetect_function, debug_collisionDetect_function_name, &debug_collisionDetect_function_desc);
+    }
+    else{
+        // Try to bind a new function with a unique name
+        bindFunction(&Debug_Domain_Collision_Detection::debug_collisionDetect_function, "123456", &debug_collisionDetect_function_desc);
+    }
     return Nebulite::Constants::ErrorTable::NONE();
 }
 const std::string Debug_Domain_Collision_Detection::debug_collisionDetect_function_name = "debug collision-detect function";
@@ -28,27 +48,75 @@ const std::string Debug_Domain_Collision_Detection::debug_collisionDetect_functi
 
 Tries to bind a function name to globalspace that is already registered, expecting a collision error.
 
-Usage: debug collision-detect function
+Usage: debug collision-detect function [fail/succeed]
+
+Defaults to fail
 )";
 
 Nebulite::Constants::Error Debug_Domain_Collision_Detection::debug_collisionDetect_category([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-    // This will fail, as the category name is already registered in GlobalSpace
-    bindCategory(debug_collisionDetect_category_name, &debug_collisionDetect_category_desc);
+    bool fail = true;
+    if(argc >= 2){
+        std::string mode = argv[1];
+        if(mode == "succeed"){
+            fail = false;
+        }
+        else if(mode == "fail"){
+            fail = true;
+        }
+        else{
+            return Nebulite::Constants::ErrorTable::FUNCTIONAL::UNKNOWN_ARG();
+        }
+    }
+
+    if(fail){
+        // This will fail, as the category name is already registered in GlobalSpace
+        bindCategory(collisionDetect_name, &debug_collisionDetect_category_desc);
+    }
+    else{
+        // Try to bind a new category with a unique name
+        bindCategory("123456", &debug_collisionDetect_category_desc);
+
+        // Just to be safe, we bind a sub-category as well
+        bindCategory("123456 789", &debug_collisionDetect_category_desc);
+    }
     return Nebulite::Constants::ErrorTable::NONE();
 }
 const std::string Debug_Domain_Collision_Detection::debug_collisionDetect_category_name = "debug collision-detect category";
-const std::string Debug_Domain_Collision_Detection::debug_collisionDetect_category_desc = R"(Tests collision detection of categorys
+const std::string Debug_Domain_Collision_Detection::debug_collisionDetect_category_desc = R"(Tests collision detection of categories
 
 Tries to bind a category that is already registered in globalspace, expecting a collision error.
 
-Usage: debug collision-detect category
+Usage: debug collision-detect category [fail/succeed]
+
+Defaults to fail
 )";
 
 Nebulite::Constants::Error Debug_Domain_Collision_Detection::debug_collisionDetect_variable([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-    // This will fail, as the variable name is already registered in GlobalSpace
-    static bool headless = false;
-    const static std::string headless_var_desc = "Indicates whether the application is running in headless mode (without GUI).";
-    bindVariable(&headless, "headless", &headless_var_desc);
+    bool fail = true;
+    if(argc >= 2){
+        std::string mode = argv[1];
+        if(mode == "succeed"){
+            fail = false;
+        }
+        else if(mode == "fail"){
+            fail = true;
+        }
+        else{
+            return Nebulite::Constants::ErrorTable::FUNCTIONAL::UNKNOWN_ARG();
+        }
+    }
+
+    if(fail){
+        // This will fail, as the variable name is already registered in GlobalSpace
+        static bool headless = false;
+        const static std::string headless_var_desc = "Indicates whether the application is running in headless mode (without GUI).";
+        bindVariable(&headless, "headless", &headless_var_desc);
+    }
+    else{
+        // Try to bind a new variable with a unique name
+        static bool testVar = false;
+        bindVariable(&testVar, "debug_collision_detect_test_variable", &debug_collisionDetect_variable_desc);
+    }
     return Nebulite::Constants::ErrorTable::NONE();
 }
 const std::string Debug_Domain_Collision_Detection::debug_collisionDetect_variable_name = "debug collision-detect variable";
@@ -56,7 +124,9 @@ const std::string Debug_Domain_Collision_Detection::debug_collisionDetect_variab
 
 Tries to bind a variable name to globalspace that is already registered, expecting a collision error.
 
-Usage: debug collision-detect variable
+Usage: debug collision-detect variable [fail/succeed]
+
+Defaults to fail
 )";
 
 }   // namespace Nebulite::DomainModule::GlobalSpace
