1544cc2b5847fd77e9c0b86e92284a6c63d9b1f0
Leo <leo.bastigkeit@gmail.com>
2025-10-31 15:13:25 +0100

    Refactor runClangTidy.sh to consolidate excluded warnings into a single variable for improved clarity and maintainability.

--- Scripts/Validation/runClangTidy.sh
+++ Scripts/Validation/runClangTidy.sh
@@ -7,12 +7,21 @@ for file in {src/*,include/*}; do
         continue
     fi
 
-    # Exclude llvmlibc checks (they produce a lot of noisy warnings about
-    # libc++/libc implementation details). Adjust as needed.
+    # Add any checks you want to exclude here
+    EXCLUDED_WARNINGS="*,-llvmlibc-*"
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-cppcoreguidelines-avoid-do-while"    # Suppress warnings about do-while loops
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-misc-include-cleaner"                # Suppress warnings about include order
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-modernize-use-trailing-return-type"  # Suppress warnings about "use auto" instead of explicit return type
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-llvm-header-guard"                   # Suppress warnings about header guards, too many false positives
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-fuchsia-default-arguments-calls"     # Suppress warnings about default arguments in function calls, this is just stupid
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-cppcoreguidelines-pro-bounds-pointer-arithmetic" # Suppress warnings about pointer arithmetic, too many false positives
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-altera-id-dependent-backward-branch" # Suppress warnings about backward branches, not relevant for us
+    EXCLUDED_WARNINGS="$EXCLUDED_WARNINGS,-misc-non-private-member-variables-in-classes"    # Suppress warnings about non-private member variables in classes
+
+    # Run clang-tidy with the specified checks and header filter
     clang-tidy "$file" \
         -p tmp/build_linux-debug \
         -header-filter='^/home/leo/Projects/Nebulite/(src|include)' \
-        -checks='*,-llvmlibc-*'
-    # -checks='-*,readability-make-member-function-const'
+        -checks="*,$EXCLUDED_WARNINGS"
 done
 
