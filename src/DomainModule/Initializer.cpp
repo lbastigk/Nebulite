fa884d9fae1bfb016b4a01d876916d263d1b1542
Leo <leo.bastigkeit@gmail.com>
2025-11-08 11:01:13 +0100

    Refactor Initializer: remove namespace prefix for improved clarity and consistency

--- src/DomainModule/Initializer.cpp
+++ src/DomainModule/Initializer.cpp
@@ -46,7 +46,7 @@ namespace Nebulite::DomainModule {
 static std::string const headless_desc = "Set headless mode (no renderer)";
 static std::string const recover_desc  = "Enable recoverable error mode";
 
-void Initializer::initGlobalSpace(Nebulite::Core::GlobalSpace* target) {
+void Initializer::initGlobalSpace(Core::GlobalSpace* target) {
     //------------------------------------------
     // Initialize DomainModules
     using namespace Nebulite::DomainModule::GlobalSpace;
@@ -62,8 +62,8 @@ void Initializer::initGlobalSpace(Nebulite::Core::GlobalSpace* target) {
     // Time module relies on knowing if anything is locking the time
     // So we need to initialize it last
     // Example: Console might want to halt time while open
-    //          if we init time first, it will update before console
-    //          thus ignoring the console's halt request being send to renderer
+    //          if we initialize time first, it will update before console
+    //          thus ignoring the console's halt request being sent to renderer
     target->initModule<Time>("Global Time Functions");
     target->initModule<Clock>("Global Clock Functions"); // Clock relies on time, so init after time
     
@@ -73,7 +73,7 @@ void Initializer::initGlobalSpace(Nebulite::Core::GlobalSpace* target) {
     target->bindVariable(&target->cmdVars.recover,  "recover",  &recover_desc);
 }
 
-void Initializer::initJSON(Nebulite::Utility::JSON* target) {
+void Initializer::initJSON(Utility::JSON* target) {
     // Initialize DomainModules
     using namespace Nebulite::DomainModule::JSON;
     target->initModule<SimpleData>("JSON Simple Data Functions");
@@ -81,7 +81,7 @@ void Initializer::initJSON(Nebulite::Utility::JSON* target) {
     target->initModule<Debug>("JSON Debug Functions");
 }
 
-void Initializer::initRenderObject(Nebulite::Core::RenderObject* target) {
+void Initializer::initRenderObject(Core::RenderObject* target) {
     // Initialize DomainModules
     using namespace Nebulite::DomainModule::RenderObject;
     target->initModule<Debug>("RenderObject Debug Functions");
@@ -90,7 +90,7 @@ void Initializer::initRenderObject(Nebulite::Core::RenderObject* target) {
     target->initModule<Mirror>("RenderObject Mirror Functions");
 }
 
-void Initializer::initRenderer(Nebulite::Core::Renderer* target) {
+void Initializer::initRenderer(Core::Renderer* target) {
     // Initialize DomainModules
     using namespace Nebulite::DomainModule::Renderer;
     target->initModule<General>("Renderer General Functions");
@@ -98,7 +98,7 @@ void Initializer::initRenderer(Nebulite::Core::Renderer* target) {
     target->initModule<RenderObjectDraft>("Renderer RenderObjectDraft Functions");
 }
 
-void Initializer::initTexture(Nebulite::Core::Texture* target) {
+void Initializer::initTexture(Core::Texture* target) {
     // Initialize DomainModules
     using namespace Nebulite::DomainModule::Texture;
     target->initModule<General>("Texture General Functions");
