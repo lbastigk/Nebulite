47b459aec5b22fb5cbacb30a86fbf2f02b7cf545
Leo <leo.bastigkeit@gmail.com>
2025-12-06 00:46:00 +0100

    Add Ruleset struct and reorganize key names in KeyNames.hpp

--- include/Constants/KeyNames.hpp
+++ include/Constants/KeyNames.hpp
@@ -54,11 +54,14 @@ const struct keyName {
      * @brief Contains all hardcoded key names used within the Nebulite::Core::RenderObject class.
      */
     const struct renderObject {
+        // Core properties
         std::string _self = "renderObject";
         std::string id = "id";
         std::string positionX = "posX";
         std::string positionY = "posY";
         std::string layer = "layer";
+
+        // Sprite properties
         std::string pixelSizeX = "sprite.sizeX";
         std::string pixelSizeY = "sprite.sizeY";
         std::string imageLocation = "sprite.link";
@@ -67,16 +70,20 @@ const struct keyName {
         std::string spritesheetSizeY = "sprite.spritesheet.sizeY";
         std::string spritesheetOffsetX = "sprite.spritesheet.offsetX";
         std::string spritesheetOffsetY = "sprite.spritesheet.offsetY";
+
+        // Interaction properties
+        std::string invokes = "invokes";                            // Todo: name "rulesets"?
+        std::string invokeSubscriptions = "invokeSubscriptions";    // TODO: name "rulesetSubscriptions"?
+
+        // Text properties
         std::string textFontsize = "text.fontSize";
         std::string textStr = "text.str";
-        std::string textDx = "text.dx";
-        std::string textDy = "text.dy";
         std::string textColorR = "text.color.R";
         std::string textColorG = "text.color.G";
         std::string textColorB = "text.color.B";
         std::string textColorA = "text.color.A";
-        std::string invokes = "invokes";
-        std::string invokeSubscriptions = "invokeSubscriptions";
+        std::string textDx = "text.dx";
+        std::string textDy = "text.dy";
     } renderObject;
 
     /**
@@ -85,16 +92,33 @@ const struct keyName {
      */
     const struct invoke {
         std::string _self = "invoke";
-        std::string logicalArg = "logicalArg";
-        std::string functioncalls_global = "functioncalls.global";
-        std::string functioncalls_self = "functioncalls.self";
-        std::string functioncalls_other = "functioncalls.other";
-        std::string topic = "topic";
-        std::string exprVector = "exprs";
         std::string typeSelf = "self";
         std::string typeOther = "other";
         std::string typeGlobal = "global";
+
+        // TODO: Use Ruleset instead for all keys below
+        std::string logicalArg = "logicalArg";
+        std::string topic = "topic";
+        std::string exprVector = "exprs";
+        std::string functioncalls_global = "functioncalls.global";
+        std::string functioncalls_self = "functioncalls.self";
+        std::string functioncalls_other = "functioncalls.other";
     } invoke;
+
+    /**
+     * @struct Ruleset
+     * @brief Contains all hardcoded key names used within the Nebulite::Interaction::Ruleset class
+     *        as well as its subclasses.
+     */
+    const struct Ruleset {
+        std::string _self = "ruleset";
+        std::string topic = "topic";
+        std::string condition = "condition";
+        std::string assignments = "assignments";
+        std::string parseOnGlobal = "functioncalls.global";
+        std::string parseOnSelf   = "functioncalls.self";
+        std::string parseOnOther  = "functioncalls.other";
+    } ruleset;
 
     /**
      * @struct Environment
