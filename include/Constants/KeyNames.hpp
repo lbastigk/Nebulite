7318a20d89fc5ad39cb5766a606bc2ae953a5a15
Leo <leo.bastigkeit@gmail.com>
2026-01-02 21:21:19 +0100

    Refactor KeyNames to improve key scoping and prepare for scopedKey integration

--- include/Constants/KeyNames.hpp
+++ include/Constants/KeyNames.hpp
@@ -7,86 +7,100 @@
 #define NEBULITE_CONSTANTS_KEYNAMES_HPP
 
 #include <string_view>
+#include "Data/Document/JsonScope.hpp"
 
 namespace Nebulite::Constants {
 
+// TODO: Use scopedKey instead of string_view for better scope safety
+// TODO: Remove unused keys and refactor used ones
+
+// Example usage:
+static constexpr Data::JsonScope::scopedKey testKey{"renderer.", "time.t"}; // Only JsonScopes with scopePrefix "renderer." can use this key
+
 struct KeyNames {
     struct Renderer {
-        inline static constexpr std::string_view self = "renderer";
-        inline static constexpr std::string_view dispResX = "display.resolution.X";
-        inline static constexpr std::string_view dispResY = "display.resolution.Y";
-        inline static constexpr std::string_view positionX = "display.position.X";
-        inline static constexpr std::string_view positionY = "display.position.Y";
-        inline static constexpr std::string_view time_t = "time.t";
-        inline static constexpr std::string_view time_t_ms = "time.t_ms";
-        inline static constexpr std::string_view time_dt = "time.dt";
-        inline static constexpr std::string_view time_dt_ms = "time.dt_ms";
+        // TODO: Use "renderer." as scope, and modify keys to be relative to that scope
+        static constexpr std::string_view self = "renderer";
+        static constexpr std::string_view dispResX = "display.resolution.X";
+        static constexpr std::string_view dispResY = "display.resolution.Y";
+        static constexpr std::string_view positionX = "display.position.X";
+        static constexpr std::string_view positionY = "display.position.Y";
+
+        // TODO: Move to scope "" for global access
+        static constexpr std::string_view time_t = "time.t";
+        static constexpr std::string_view time_t_ms = "time.t_ms";
+        static constexpr std::string_view time_dt = "time.dt";
+        static constexpr std::string_view time_dt_ms = "time.dt_ms";
     };
 
     struct RNGs {
-        inline static constexpr std::string_view A = "random.A";
-        inline static constexpr std::string_view B = "random.B";
-        inline static constexpr std::string_view C = "random.C";
-        inline static constexpr std::string_view D = "random.D";
-        inline static constexpr std::string_view min = "random.min";
-        inline static constexpr std::string_view max = "random.max";
+        // TODO: Use "random." as scope, and modify keys to be relative to that scope
+        static constexpr std::string_view A = "random.A";
+        static constexpr std::string_view B = "random.B";
+        static constexpr std::string_view C = "random.C";
+        static constexpr std::string_view D = "random.D";
+        static constexpr std::string_view min = "random.min";
+        static constexpr std::string_view max = "random.max";
     };
 
     struct RenderObject {
-        inline static constexpr std::string_view self = "renderObject";
-        inline static constexpr std::string_view id = "id";
-        inline static constexpr std::string_view positionX = "posX";
-        inline static constexpr std::string_view positionY = "posY";
-        inline static constexpr std::string_view layer = "layer";
+        // TODO: Use "" as scope, and modify keys to be relative to that scope
+        static constexpr std::string_view self = "renderObject";
+        static constexpr std::string_view id = "id";
+        static constexpr std::string_view positionX = "posX";
+        static constexpr std::string_view positionY = "posY";
+        static constexpr std::string_view layer = "layer";
 
-        inline static constexpr std::string_view pixelSizeX = "sprite.sizeX";
-        inline static constexpr std::string_view pixelSizeY = "sprite.sizeY";
-        inline static constexpr std::string_view imageLocation = "sprite.link";
-        inline static constexpr std::string_view isSpritesheet = "sprite.spritesheet.isSpritesheet";
-        inline static constexpr std::string_view spritesheetSizeX = "sprite.spritesheet.sizeX";
-        inline static constexpr std::string_view spritesheetSizeY = "sprite.spritesheet.sizeY";
-        inline static constexpr std::string_view spritesheetOffsetX = "sprite.spritesheet.offsetX";
-        inline static constexpr std::string_view spritesheetOffsetY = "sprite.spritesheet.offsetY";
+        // TODO: Use "texture." as scope
+        static constexpr std::string_view pixelSizeX = "sprite.sizeX";
+        static constexpr std::string_view pixelSizeY = "sprite.sizeY";
+        static constexpr std::string_view imageLocation = "sprite.link";
+        static constexpr std::string_view isSpritesheet = "sprite.spritesheet.isSpritesheet";
+        static constexpr std::string_view spritesheetSizeX = "sprite.spritesheet.sizeX";
+        static constexpr std::string_view spritesheetSizeY = "sprite.spritesheet.sizeY";
+        static constexpr std::string_view spritesheetOffsetX = "sprite.spritesheet.offsetX";
+        static constexpr std::string_view spritesheetOffsetY = "sprite.spritesheet.offsetY";
+        static constexpr std::string_view textFontsize = "text.fontSize";
+        static constexpr std::string_view textStr = "text.str";
+        static constexpr std::string_view textColorR = "text.color.R";
+        static constexpr std::string_view textColorG = "text.color.G";
+        static constexpr std::string_view textColorB = "text.color.B";
+        static constexpr std::string_view textColorA = "text.color.A";
+        static constexpr std::string_view textDx = "text.dx";
+        static constexpr std::string_view textDy = "text.dy";
 
-        inline static constexpr std::string_view invokes = "invokes";
-        inline static constexpr std::string_view invokeSubscriptions = "invokeSubscriptions";
-
-        inline static constexpr std::string_view textFontsize = "text.fontSize";
-        inline static constexpr std::string_view textStr = "text.str";
-        inline static constexpr std::string_view textColorR = "text.color.R";
-        inline static constexpr std::string_view textColorG = "text.color.G";
-        inline static constexpr std::string_view textColorB = "text.color.B";
-        inline static constexpr std::string_view textColorA = "text.color.A";
-        inline static constexpr std::string_view textDx = "text.dx";
-        inline static constexpr std::string_view textDy = "text.dy";
+        // TODO: Are these still used?
+        static constexpr std::string_view invokes = "invokes";
+        static constexpr std::string_view invokeSubscriptions = "invokeSubscriptions";
     };
 
     struct Invoke {
-        inline static constexpr std::string_view self = "invoke";
-        inline static constexpr std::string_view typeSelf = "self";
-        inline static constexpr std::string_view typeOther = "other";
-        inline static constexpr std::string_view typeGlobal = "global";
+        // TODO: Are these still used?
+        static constexpr std::string_view self = "invoke";
+        static constexpr std::string_view typeSelf = "self";
+        static constexpr std::string_view typeOther = "other";
+        static constexpr std::string_view typeGlobal = "global";
 
-        inline static constexpr std::string_view logicalArg = "logicalArg";
-        inline static constexpr std::string_view topic = "topic";
-        inline static constexpr std::string_view exprVector = "exprs";
-        inline static constexpr std::string_view functioncalls_global = "functioncalls.global";
-        inline static constexpr std::string_view functioncalls_self = "functioncalls.self";
-        inline static constexpr std::string_view functioncalls_other = "functioncalls.other";
+        static constexpr std::string_view logicalArg = "logicalArg";
+        static constexpr std::string_view topic = "topic";
+        static constexpr std::string_view exprVector = "exprs";
+        static constexpr std::string_view functioncalls_global = "functioncalls.global";
+        static constexpr std::string_view functioncalls_self = "functioncalls.self";
+        static constexpr std::string_view functioncalls_other = "functioncalls.other";
     };
 
     struct Ruleset {
-        inline static constexpr std::string_view self = "ruleset";
-        inline static constexpr std::string_view topic = "topic";
-        inline static constexpr std::string_view condition = "condition";
-        inline static constexpr std::string_view assignments = "assignments";
-        inline static constexpr std::string_view parseOnGlobal = "functioncalls.global";
-        inline static constexpr std::string_view parseOnSelf   = "functioncalls.self";
-        inline static constexpr std::string_view parseOnOther  = "functioncalls.other";
+        static constexpr std::string_view self = "ruleset";
+        static constexpr std::string_view topic = "topic";
+        static constexpr std::string_view condition = "condition";
+        static constexpr std::string_view assignments = "assignments";
+        static constexpr std::string_view parseOnGlobal = "functioncalls.global";
+        static constexpr std::string_view parseOnSelf   = "functioncalls.self";
+        static constexpr std::string_view parseOnOther  = "functioncalls.other";
     };
 
     struct Environment {
-        inline static constexpr std::string_view renderObjectContainer = "RenderObjectContainer";
+        static constexpr std::string_view renderObjectContainer = "RenderObjectContainer";
     };
 };
 
