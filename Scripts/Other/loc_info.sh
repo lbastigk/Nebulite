c9d7232a5dca3105ef7b36fd8c496dd9294e5b32
Leo <leo.bastigkeit@gmail.com>
2026-01-03 23:11:51 +0100

    Refactor loc_info.sh to streamline lines of code reporting and enhance cloc settings; update cloc_lang_define.txt for JSON and JSONC support

--- Scripts/Other/loc_info.sh
+++ Scripts/Other/loc_info.sh
@@ -6,26 +6,24 @@ if [ "$(basename "$PWD")" != "Nebulite" ]; then
     exit 1
 fi
 
+
 # Inform about lines of code:
+# Argument 1: directory list
+# Argument 2: label
+function print_loc_for_dirs {
+    DIRS=$1
+    LABEL=$2
+
+    CLOC_SETTINGS="--force-lang-def=./Tools/cloc_lang_define.txt $DIRS"
+    echo "Lines of code for $LABEL:"
+    #cloc $CLOC_SETTINGS 2>/dev/null # Unused, too detailed
+    echo ""
+    cloc $CLOC_SETTINGS --csv 2>/dev/null | tail -1 | awk -F',' '{total=$3+$4+$5; print "Total lines (including comments and blanks): " total}' 2>/dev/null
+    echo ""
+}
 
 # Custom lang define, counting:
-# - custom .nebs files
-# - c++ source files
-# - c++ header files
-# - .sh and .py scripts
-# - custom external for SDL compilation
-CLOC_SETTINGS="--force-lang-def=./Tools/cloc_lang_define.txt \
-    Resources/ \
-    TaskFiles/ \
-    src/ \
-    include/ \
-    Scripts/ \
-    external/SDL_Crossplatform_Local/Scripts/ \
-    external/SDL_Crossplatform_Local/src/"
-echo ""
-echo ""
-echo "Lines of code for Project + Tests:"
-cloc $CLOC_SETTINGS 2>/dev/null
-echo ""
-cloc $CLOC_SETTINGS --csv 2>/dev/null | tail -1 | awk -F',' '{total=$3+$4+$5; print "Total lines (including comments and blanks): " total}' 2>/dev/null
-echo ""
+print_loc_for_dirs "./src ./include" "Source Code (C, C++)"
+print_loc_for_dirs "./Scripts" "Scripts (Python, Bash)"
+print_loc_for_dirs "./Tools/Tests/ ./Tools/tests.jsonc ./TaskFiles/" "Tests (JSON, JSONC, nebs)"
+print_loc_for_dirs "./Resources" "Resources (JSON, JSOC)"
