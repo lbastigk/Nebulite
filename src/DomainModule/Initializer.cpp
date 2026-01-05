dcda423677b8e22ba689ae18beb3124011c4ba45
Leo <leo.bastigkeit@gmail.com>
2026-01-05 19:20:27 +0100

    Refactor Initializer to use scoped document prefixes for improved organization and clarity in module initialization

--- src/DomainModule/Initializer.cpp
+++ src/DomainModule/Initializer.cpp
@@ -46,21 +46,29 @@
 //------------------------------------------
 namespace Nebulite::DomainModule {
 
+// We use the scope __tmp__.<name> to indicate modules with restricted access,
+// that don't require any document.
+// If we ever see __tmp__ being used, we know that we wrote to the document even though the DomainModule is designed to not do so.
+// This doesnt break anything, but is a useful indicator that:
+// - we wrote to the document when it was designed to not do so
+// - we may want to rename the scope to something more appropriate
+
 void Initializer::initGlobalSpace(Core::GlobalSpace* target) {
+
     //------------------------------------------
     // Initialize DomainModules
     using namespace Nebulite::DomainModule::GlobalSpace;
     target->initModule<General>("Global General Functions", &target->shareDocumentScopeBase(""));
-    target->initModule<Debug>("Global Debug Functions", &target->shareDocumentScopeBase(""));
-    target->initModule<Ruleset>("Global Ruleset Functions", &target->shareDocumentScopeBase(""));
+    target->initModule<Debug>("Global Debug Functions", &target->shareDocumentScopeBase("debug."));
+    target->initModule<Ruleset>("Global Ruleset Functions", &target->shareDocumentScopeBase("ruleset."));
 
     //------------------------------------------
     // Special debugging utilities
-    target->initModule<FunctionCollision>("Global Function Collision Detection utilities", &target->shareDocumentScopeBase(""));
+    target->initModule<FunctionCollision>("Global Function Collision Detection utilities", &target->shareDocumentScopeBase("__tmp__.function_collision."));
 
     //------------------------------------------
     // Feature Test Modules
-    target->initModule<FeatureTest>("Global Feature Test Functions", &target->shareDocumentScopeBase(""));
+    target->initModule<FeatureTest>("Global Feature Test Functions", &target->shareDocumentScopeBase("__tmp__.feature_test."));
 
     //------------------------------------------
     // Time module relies on knowing if anything is locking the time
@@ -68,8 +76,8 @@ void Initializer::initGlobalSpace(Core::GlobalSpace* target) {
     // Example: Console might want to halt time while open
     //          if we initialize time first, it will update before console
     //          thus ignoring the console's halt request being sent to renderer
-    target->initModule<Time>("Global Time Functions", &target->shareDocumentScopeBase(""));
-    target->initModule<Clock>("Global Clock Functions", &target->shareDocumentScopeBase("")); // Clock relies on time, so init after time
+    target->initModule<Time>("Global Time Functions", &target->shareDocumentScopeBase("time."));
+    target->initModule<Clock>("Global Clock Functions", &target->shareDocumentScopeBase("clock.")); // Clock relies on time, so init after time
 
     //------------------------------------------
     // Initialize Variable Bindings
@@ -91,7 +99,7 @@ void Initializer::initRenderObject(Core::RenderObject* target) {
     target->initModule<Debug>("RenderObject Debug Functions", &target->shareDocumentScopeBase(""));
     target->initModule<Logging>("RenderObject Logging Functions", &target->shareDocumentScopeBase(""));
     target->initModule<Mirror>("RenderObject Mirror Functions", &target->shareDocumentScopeBase(""));
-    target->initModule<Ruleset>("RenderObject Ruleset Functions", &target->shareDocumentScopeBase(""));
+    target->initModule<Ruleset>("RenderObject Ruleset Functions", &target->shareDocumentScopeBase("ruleset."));
     target->initModule<StateUpdate>("RenderObject State Update Functions", &target->shareDocumentScopeBase(""));
 }
 
