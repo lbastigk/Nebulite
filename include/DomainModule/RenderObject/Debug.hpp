50a773d35b480256603723de78d2f263f1b6988f
Leo <leo.bastigkeit@gmail.com>
2026-01-17 03:52:49 +0100

    refactor: Add [[nodiscard]] attribute to multiple error-returning functions for improved code safety

--- include/DomainModule/RenderObject/Debug.hpp
+++ include/DomainModule/RenderObject/Debug.hpp
@@ -47,7 +47,7 @@ public:
         "eval echo $(1+1)    outputs:    2.000000\n"
         "eval echo {self.id} outputs this objects id\n";
 
-    Constants::Error printSrcRect() const ;
+    [[nodiscard]] Constants::Error printSrcRect() const ;
     static auto constexpr printSrcRect_name = "debug print-src-rect";
     static auto constexpr printSrcRect_desc = "Prints debug information about the source rectangle to console\n"
         "\n"
@@ -58,7 +58,7 @@ public:
         "If the RenderObject is not a spritesheet, indicates that instead:\n"
         "This RenderObject is not a spritesheet.\n";
 
-    Constants::Error printDstRect() const ;
+    [[nodiscard]] Constants::Error printDstRect() const ;
     static auto constexpr printDstRect_name = "debug print-dst-rect";
     static auto constexpr printDstRect_desc = "Prints debug information about the destination rectangle to console\n"
         "\n"
