dc68a3e58c8cf1b34eb920501441a9143a8927f7
Leo <leo.bastigkeit@gmail.com>
2026-01-19 09:13:18 +0100

    feat: Add empty GUI test module for the NEBULITE engine

---
+++ include/DomainModule/Renderer/GUI_test.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file GUI_test.hpp
+ * @brief GUI test module for the NEBULITE engine.
+ */
+
+#ifndef NEBULITE_RRDM_GUI_TEST_HPP
+#define NEBULITE_RRDM_GUI_TEST_HPP
+
+//------------------------------------------
+// Includes
+
+// External
+#include "imgui.h"
+
+// Nebulite
+#include "Constants/ErrorTypes.hpp"
+#include "Interaction/Execution/DomainModule.hpp"
+
+//------------------------------------------
+// Forward declarations
+namespace Nebulite::Core {
+class Renderer;
+} // namespace Nebulite::Core
+
+//------------------------------------------
+namespace Nebulite::DomainModule::Renderer {
+/**
+ * @class Nebulite::DomainModule::Renderer::GUI_test
+ * @brief DomainModule for testing GUI functionalities.
+ */
+NEBULITE_DOMAINMODULE(Nebulite::Core::Renderer, GUI_test) {
+public:
+    Constants::Error update() override;
+    void reinit() override {}
+
+    //------------------------------------------
+    // Available Functions
+
+
+    //------------------------------------------
+    // Setup
+
+    /**
+     * @brief Initializes the module, binding functions and variables.
+     */
+    NEBULITE_DOMAINMODULE_CONSTRUCTOR(Nebulite::Core::Renderer, GUI_test) {
+
+    }
+
+private:
+
+};
+} // namespace Nebulite::DomainModule::Renderer
+#endif // NEBULITE_RRDM_GUI_TEST_HPP
