3fde1c04833d2c2c6267890d76502fe086c81a6d
Leo <leo.bastigkeit@gmail.com>
2026-01-23 11:19:26 +0100

    feat: Enhance Drawcall class with detailed TODOs for RenderObject integration

--- include/Graphics/Drawcall.hpp
+++ include/Graphics/Drawcall.hpp
@@ -1,6 +1,17 @@
 /**
  * @file Drawcall.hpp
  * @brief This file contains the declaration of the Nebulite::Graphics::Drawcall base class.
+ * @todo The big question currently is how to best implement the structure into a RenderObject.
+ *       RenderObjects should be able to hold multiple Drawcalls, with a hashmap where the hashmap key is also the key in the JsonScope.
+ *       This way, we can easily access and modify specific drawcalls within a RenderObject.
+ *       E.g.: on-drawcall parse <args>
+ *       But should each drawcall hold its Texture?
+ *       It makes sense in a way, as any drawcall needs its own texture data.
+ *       This way, a drawcall would seemingly be just a wrapper around a texture to better integrate with the rendering pipeline.
+ *       For this to work properly, we would need a way to easily register all available drawcalls within a RenderObject.
+ *       ==================================================================================
+ *       -> We need a way to list all available keys in a JsonScope! This now has priority.
+ *       ==================================================================================
  */
 
 #ifndef NEBULITE_GRAPHICS_DRAWCALL_HPP
@@ -42,13 +53,17 @@ public:
     // Current idea: Depending on the type of drawcall, we have different draw implementations
     // Since there are different types of drawcalls, it might be better to have separate derived classes for each type
     // But it would be more complex, as any change in the workspace structure could potentially require the class to change.
+    // Basically: static auto tr = timedRoutine(checkType()); tr.update(); time switch(type) { case SPRITE: drawSprite(); break; case TEXT: drawText(); break; };
     void draw();
+
+    // Parse a string onto the texture
+    Constants::Error parseStr(std::string str);
 protected:
     struct Key {
         static auto constexpr type = Data::ScopedKeyView::createUnscoped("type");
     };
 
-    enum Type {
+    enum Type { // TODO: We could determine the type on each draw call, or use some timing mechanism
         SPRITE,
         TEXT
     }type;
