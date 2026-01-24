0923ce859b0409f58b69f87f8e211f2ed92ce6b4
Leo <leo.bastigkeit@gmail.com>
2026-01-24 22:26:39 +0100

    fix: Correct drawcall scope string in RenderObject for proper initialization

--- src/Core/RenderObject.cpp
+++ src/Core/RenderObject.cpp
@@ -26,7 +26,7 @@ void setStandardValues(JsonScope& document) {
     document.set(Constants::KeyNames::RenderObject::layer, 0);
 
     // Create a basic drawcall
-    Graphics::Drawcall::setDefaultTypeSprite(document.shareScope(Constants::KeyNames::RenderObject::draw + "core"));
+    Graphics::Drawcall::setDefaultTypeSprite(document.shareScope(Constants::KeyNames::RenderObject::draw + ".core"));
 
     // Ruleset
     document.setEmptyArray(Constants::KeyNames::RenderObject::Ruleset::broadcast);
