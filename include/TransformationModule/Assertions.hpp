5ff694d3a56863ef2a014d3750de6f152a4ae96f
Leo <leo.bastigkeit@gmail.com>
2026-01-29 19:38:17 +0100

    feat: Add type assertions for object, array, and basic value with user-defined error messages

--- include/TransformationModule/Assertions.hpp
+++ include/TransformationModule/Assertions.hpp
@@ -20,6 +20,10 @@ public:
     //------------------------------------------
     // Available Transformations
 
+    // TODO: categorizing would be nice:
+    //       assert nonEmpty, assert type object/array/value, assert range, assert regex, etc.
+    //       implement bindCategory in TransformationModule base class!
+
     static bool assertNonEmpty(std::span<std::string const> const& args, Core::JsonScope* jsonDoc);
     static auto constexpr assertNonEmptyName = "assertNonEmpty";
     static auto constexpr assertNonEmptyDesc = "Asserts that the current JSON value is non-empty.\n"
@@ -27,7 +31,28 @@ public:
         "Accepts an optional user-defined error message as additional arguments.\n"
         "Usage: |assertNonEmpty -> {value,<Exception thrown if empty>}\n";
 
-    // TODO: assertType, assertRange, assertRegex, etc.
+    static bool assertTypeObject(std::span<std::string const> const& args, Core::JsonScope* jsonDoc);
+    static auto constexpr assertTypeObjectName = "assertTypeObject";
+    static auto constexpr assertTypeObjectDesc = "Asserts that the current JSON value is of type object.\n"
+        "If the value is not an object, the transformation fails and the program exits\n"
+        "Accepts an optional user-defined error message as additional arguments.\n"
+        "Usage: |assertTypeObject -> {value,<Exception thrown if not object>}\n";
+
+    static bool assertTypeArray(std::span<std::string const> const& args, Core::JsonScope* jsonDoc);
+    static auto constexpr assertTypeArrayName = "assertTypeArray";
+    static auto constexpr assertTypeArrayDesc = "Asserts that the current JSON value is of type array.\n"
+        "If the value is not an array, the transformation fails and the program exits\n"
+        "Accepts an optional user-defined error message as additional arguments.\n"
+        "Usage: |assertTypeArray -> {value,<Exception thrown if not array>}\n";
+
+    static bool assertTypeBasicValue(std::span<std::string const> const& args, Core::JsonScope* jsonDoc);
+    static auto constexpr assertTypeValueName = "assertTypeBasicValue";
+    static auto constexpr assertTypeValueDesc = "Asserts that the current JSON value is a basic value (not object or array or null).\n"
+        "If the value is not a basic value, the transformation fails and the program exits\n"
+        "Accepts an optional user-defined error message as additional arguments.\n"
+        "Usage: |assertTypeValue -> {value,<Exception thrown if not value>}\n";
+
+    // TODO: assertRegex perhaps?
 
     // TODO: Another class for silent assertions that do not exit the program, but return false on failure
     //       e.g., requiresNonEmpty, requiresType, requiresRange, requiresRegex, etc.
--- src/TransformationModule/Assertions.cpp
+++ src/TransformationModule/Assertions.cpp
@@ -19,11 +19,41 @@ bool Assertions::assertNonEmpty(std::span<std::string const> const& args, Core::
     return true;
 }
 
+// NOLINTNEXTLINE
+bool Assertions::assertTypeObject(std::span<std::string const> const& args, Core::JsonScope* jsonDoc) {
+    if (jsonDoc->memberType(valueKey) != Data::KeyType::object) {
+        printUserDefinedMessage(args);
+        static std::string errorMessage = std::string(__FUNCTION__) + " JSON value is not an object";
+        throw std::runtime_error(errorMessage);
+    }
+    return true;
+}
+
+// NOLINTNEXTLINE
+bool Assertions::assertTypeArray(std::span<std::string const> const& args, Core::JsonScope* jsonDoc) {
+    if (jsonDoc->memberType(valueKey) != Data::KeyType::array) {
+        printUserDefinedMessage(args);
+        static std::string errorMessage = std::string(__FUNCTION__) + " JSON value is not an array";
+        throw std::runtime_error(errorMessage);
+    }
+    return true;
+}
+
+// NOLINTNEXTLINE
+bool Assertions::assertTypeBasicValue(std::span<std::string const> const& args, Core::JsonScope* jsonDoc) {
+    if (jsonDoc->memberType(valueKey) != Data::KeyType::value) {
+        printUserDefinedMessage(args);
+        static std::string errorMessage = std::string(__FUNCTION__) + " JSON value is not a basic value";
+        throw std::runtime_error(errorMessage);
+    }
+    return true;
+}
+
 void Assertions::printUserDefinedMessage(std::span<std::string const> const& args){
     if (args.size() < 2) {
-        return;
+        return; // No message provided
     }
-    Nebulite::Error::println(Utility::StringHandler::recombineArgs(args.subspan(1)));
+    Error::println(Utility::StringHandler::recombineArgs(args.subspan(1)));
 }
 
 } // namespace Nebulite::TransformationModule
