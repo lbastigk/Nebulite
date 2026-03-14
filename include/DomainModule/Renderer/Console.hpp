3f8d7c0e5363e381b5f07ccab327ba120c9366eb
Leo <leo.bastigkeit@gmail.com>
2026-03-14 17:26:29 +0100

    Removed unnecessary autotype functionality

--- include/DomainModule/Renderer/Console.hpp
+++ include/DomainModule/Renderer/Console.hpp
@@ -9,18 +9,11 @@
 //------------------------------------------
 // Includes
 
-// Standard Library
-#include <queue>
-
-// External
-#include <SDL3/SDL.h>
-
 // Nebulite
 #include "Constants/ErrorTypes.hpp"
 #include "Constants/KeyNames.hpp"
 #include "Interaction/Execution/DomainModule.hpp"
 #include "Utility/TextInput.hpp"
-#include "Utility/TimeKeeper.hpp"
 
 //------------------------------------------
 // Forward declarations
@@ -55,58 +48,6 @@ public:
         "\n"
         "Usage: console close\n";
 
-    // Autotype functions
-
-    Constants::Error consoleAutotypeText(std::span<std::string const> const& args);
-    static auto constexpr consoleAutotypeText_name = "console autotype text";
-    static auto constexpr consoleAutotypeText_desc = "Adds a text input command into the autotype queue.\n"
-        "\n"
-        "Usage: console autotype text <text>\n";
-
-    Constants::Error consoleAutotypeEnter();
-    static auto constexpr consoleAutotypeEnter_name = "console autotype enter";
-    static auto constexpr consoleAutotypeEnter_desc = "Puts an enter command into the autotype queue.\n"
-        "\n"
-        "Usage: console autotype enter\n";
-
-    Constants::Error consoleAutotypeExecute();
-    static auto constexpr consoleAutotypeExecute_name = "console autotype execute";
-    static auto constexpr consoleAutotypeExecute_desc = "Executes all autotype commands in the queue.\n"
-        "\n"
-        "Usage: console autotype execute\n";
-
-    Constants::Error consoleAutotypeWait(std::span<std::string const> const& args);
-    static auto constexpr consoleAutotypeWait_name = "console autotype wait";
-    static auto constexpr consoleAutotypeWait_desc = "Waits for a specified amount of ms before executing the next autotype command.\n"
-        "\n"
-        "Usage: console autotype wait <milliseconds>\n";
-
-    Constants::Error consoleAutotypeClose();
-    static auto constexpr consoleAutotypeClose_name = "console autotype close";
-    static auto constexpr consoleAutotypeClose_desc = "Closes the console if the autotype wait counter reaches zero"
-        "\n"
-        "Usage: console autotype close\n";
-
-    Constants::Error consoleAutotypeHistoryUp();
-    static auto constexpr consoleAutotypeHistoryUp_name = "console autotype up";
-    static auto constexpr consoleAutotypeHistoryUp_desc = "Adds a history up command into the autotype queue.\n"
-        "\n"
-        "Usage: console autotype up\n";
-
-    Constants::Error consoleAutotypeHistoryDown();
-    static auto constexpr consoleAutotypeHistoryDown_name = "console autotype down";
-    static auto constexpr consoleAutotypeHistoryDown_desc = "Adds a history down command into the autotype queue.\n"
-        "\n"
-        "Usage: console autotype down\n";
-
-    Constants::Error consoleAutotypeClear();
-    static auto constexpr consoleAutotypeClear_name = "console autotype clear";
-    static auto constexpr consoleAutotypeClear_desc = "Clears the autotype queue without executing the commands.\n"
-        "Clears both the queue pending for execution and the active queue currently being executed.\n"
-        "\n"
-        "Usage: console autotype clear\n";
-
-
     //------------------------------------------
     // Category strings
 
@@ -114,9 +55,6 @@ public:
     static auto constexpr console_desc = "Console commands and settings.\n"
         "\n"
         "Contains commands to manipulate the in-application console.\n";
-
-    static auto constexpr consoleAutotype_name = "console autotype";
-    static auto constexpr consoleAutotype_desc = "Utilities to automate typing in the console";
 
     //------------------------------------------
     // Setup
@@ -130,16 +68,6 @@ public:
         bindCategory(console_name, console_desc);
         BIND_FUNCTION(&Console::consoleOpen, consoleOpen_name, consoleOpen_desc);
         BIND_FUNCTION(&Console::consoleClose, consoleClose_name, consoleClose_desc);
-
-        bindCategory(consoleAutotype_name, consoleAutotype_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeText, consoleAutotypeText_name, consoleAutotypeText_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeEnter, consoleAutotypeEnter_name, consoleAutotypeEnter_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeExecute, consoleAutotypeExecute_name, consoleAutotypeExecute_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeWait, consoleAutotypeWait_name, consoleAutotypeWait_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeClose, consoleAutotypeClose_name, consoleAutotypeClose_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeHistoryUp, consoleAutotypeHistoryUp_name, consoleAutotypeHistoryUp_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeHistoryDown, consoleAutotypeHistoryDown_name, consoleAutotypeHistoryDown_desc);
-        BIND_FUNCTION(&Console::consoleAutotypeClear, consoleAutotypeClear_name, consoleAutotypeClear_desc);
     }
 
     struct Key : Data::KeyGroup<"renderer."> {
@@ -149,48 +77,6 @@ public:
 private:
     // Whether the console is currently active
     bool consoleMode = false;
-
-    //------------------------------------------
-    // Autotype handling
-
-    /**
-     * @brief Struct to manage the autotype system, which allows queuing up commands to be executed in the console with optional wait times between them.
-     */
-    struct AutoType {
-        /**
-         * @struct Command
-         * @brief Represents a command for the autotype system
-         * @details Commands cannot be executed immediately, so we store their representation in a queue instead.
-         */
-        struct Command {
-            enum class Type {
-                TEXT,
-                ENTER,
-                CLOSE,
-                WAIT,
-                HISTORY_UP,
-                HISTORY_DOWN
-            } type;
-            std::string text; // Additional data for text or wait commands
-        };
-
-        // Stores all autotype commands before "autotype execute" is called
-        std::queue<Command> queue;
-
-        // Stores autotype commands that are currently being executed (after "autotype execute" is called, until all commands are executed)
-        std::queue<Command> activeQueue;
-
-        // Remaining wait time in ms before the next autotype command from autotypeActiveQueue can be executed.
-        size_t waitTimeRemaining = 0;
-
-        // Timer to track dt for autotype wait commands
-        Utility::TimeKeeper waitTimer;
-    }autoType;
-
-    /**
-     * @brief Processes the autotype command queue, executing commands one by one while respecting wait times.
-     */
-    void processAutotypeQueue();
 };
 } // namespace Nebulite::DomainModule::Renderer
 #endif // NEBULITE_DOMAINMODULE_RENDERER_CONSOLE_HPP
--- src/DomainModule/Renderer/Console.cpp
+++ src/DomainModule/Renderer/Console.cpp
@@ -1,12 +1,8 @@
 //------------------------------------------
 // Includes
 
-// External
-#include <imgui.h>
-
 // Nebulite
 #include "Nebulite.hpp"
-#include "Constants/KeyNames.hpp"
 #include "Core/Renderer.hpp"
 #include "DomainModule/Renderer/Console.hpp"
 #include "DomainModule/Renderer/Input.hpp"
@@ -16,9 +12,6 @@
 namespace Nebulite::DomainModule::Renderer {
 
 Constants::Error Console::update() {
-    // Execute autoType commands
-    processAutotypeQueue();
-
     // Toggling console mode
     static auto const toggleKey = Input::Key::keyboardDelta + "tab";
     if (moduleScope.get<int>(toggleKey).value_or(0) == 1) {
@@ -47,52 +40,6 @@ Constants::Error Console::update() {
     //------------------------------------------
     // Return
     return Constants::ErrorTable::NONE();
-}
-
-void Console::processAutotypeQueue() {
-    autoType.waitTimer.update();
-    auto const dt_ms = autoType.waitTimer.get_dt_ms();
-    if (autoType.waitTimeRemaining > 0) {
-        if (dt_ms >= autoType.waitTimeRemaining) {
-            autoType.waitTimeRemaining = 0;
-        } else {
-            autoType.waitTimeRemaining -= dt_ms;
-        }
-    } else {
-        while (!autoType.activeQueue.empty()) {
-            if (autoType.waitTimeRemaining > 0) {
-                break; // Wait time set by a command, pause execution of further commands until next update
-            }
-            switch (auto const& [type, text] = autoType.activeQueue.front(); type) {
-            case AutoType::Command::Type::TEXT:
-                // TODO: Insert text into Imgui input buffer
-                break;
-            case AutoType::Command::Type::ENTER:
-                // TODO: Insert submit into Imgui input buffer
-                break;
-            case AutoType::Command::Type::CLOSE:
-                consoleMode = false;
-                SDL_StopTextInput(domain.getSdlWindow());
-                break;
-            case AutoType::Command::Type::WAIT:
-                try {
-                    autoType.waitTimeRemaining = std::stoul(text);
-                } catch (std::exception const&) {
-                    domain.capture.error.println("Invalid wait time in autoType command: ", text);
-                }
-                break;
-            case AutoType::Command::Type::HISTORY_UP:
-                // TODO: Inject events into the imgui pipeline
-                break;
-            case AutoType::Command::Type::HISTORY_DOWN:
-                // TODO: Inject events into the imgui pipeline
-                break;
-            default:
-                std::unreachable();
-            }
-            autoType.activeQueue.pop();
-        }
-    }
 }
 
 } // namespace Nebulite::DomainModule::GlobalSpace::Console
