e91d5769f8d413702e0a2c67296e8eb10cfffa94
Leo <leo.bastigkeit@gmail.com>
2026-04-13 15:43:05 +0200

    Add todos for data-reflect and proper element deletion

--- src/Module/RmlUi/ExpressionManager.cpp
+++ src/Module/RmlUi/ExpressionManager.cpp
@@ -92,7 +92,8 @@ void ExpressionManager::OnElementCreate(Rml::Element* element) {
 
 void ExpressionManager::OnElementDestroy(Rml::Element* element) {
     (void)element;
-    // TODO: doesn't work:
+    // TODO: doesn't work: We cannot remove elements on destroy, as we might be looping through the maps!
+    //       Instead, mark deleted and delete on update
     /*
     for (auto elementMap: expressions | std::views::values) {
         if (elementMap.find(element) != elementMap.end()) {
@@ -102,12 +103,26 @@ void ExpressionManager::OnElementDestroy(Rml::Element* element) {
     */
 }
 
-// TODO: Add attributes such as data-reflect that loop through each member of a given scope to, for example, dynamically generate lists
+
 void ExpressionManager::compileDocument(Rml::ElementDocument* root, Rml::Element* element, size_t const& depth) {
     if (!element) return;
     if (element->GetAttribute("data-eval")) {
         // Nebulite Expression can handle text, so we don't need to sanitize the inner RML in any way.
         expressions[root].emplace(element, Interaction::Logic::Expression(element->GetInnerRML()));
+    }
+    else if (element->GetAttribute("data-reflect")) {
+        // TODO: Add attributes such as data-reflect that loop through each member of a given scope to, for example, dynamically generate lists
+        // Idea: duplicate any inner rml for each member. The only issue is how we pass the correct context to each element. We need a way to store the context for each element.
+        // The only way to also get the member key is to copy the values on each update?
+        // e.g.: Reflect on global.obj with members a,b,c
+        // <p data-reflect="global.obj">
+        //   <p data-eval="true">
+        //     Member is: {self.key} with value: {self.value}
+        //   </p>
+        // </p>
+        // Would expand on every update
+        auto key = std::string(element->GetAttribute("data-reflect")->Get<Rml::String>());
+        capture.warning.println("Data attribute data-reflect is not yet supported. Tried reflect on key: ", key);
     }
     else { // Do not evaluate any inner elements, as this could cause issues with pointers: parent element gets re-evaluated, potentially breaking element pointers of children.
         auto const count = static_cast<size_t>(element->GetNumChildren());
