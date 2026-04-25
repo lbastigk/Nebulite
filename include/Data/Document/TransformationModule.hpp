d973e0332edf1d9e6c371949a73ff6d717ffce97
Leo <leo.bastigkeit@gmail.com>
2026-04-25 17:54:32 +0200

    Add documentation, remove unnecessary code and add a proper compile-time-check for rootKeyStr

--- include/Data/Document/TransformationModule.hpp
+++ include/Data/Document/TransformationModule.hpp
@@ -32,26 +32,37 @@ public:
 
     virtual ~TransformationModule();
 
+    /**
+     * @brief Binds a static function to the transformation funcTree
+     * @tparam Func The function type to bind
+     * @param functionPtr The function to bind
+     * @param name The name of the function
+     * @param helpDescription The help description of the function
+     */
     template <typename Func>
     void bindTransformation(Func functionPtr, std::string_view const& name, std::string_view const& helpDescription) const {
         Interaction::Execution::DomainModuleBase::bindFunctionStatic(transformationFuncTree.get(), functionPtr, name, helpDescription);
     }
 
+    /**
+     * @brief Binds a category to the transformation funcTree
+     * @param name The name of the category
+     * @param helpDescription The help description of the category
+     */
     void bindCategory(std::string_view const& name, std::string_view const& helpDescription) const {
         transformationFuncTree->bindCategory(name, helpDescription);
     }
 
-    virtual void bindTransformations() {
-        // Basic example of how to bind transformations
-        bindTransformation(&TransformationModule::bar, "test1", "Example bind of a static function.\n");
-    }
+    /**
+     * @brief Add all functions to bind here
+     */
+    virtual void bindTransformations() {}
 
     /**
      * @brief The key string used to store and retrieve the value being modified.
      * @details Uses an empty string as key, so the entire JSON document is the value used.
-     * @note DO NOT CHANGE THIS KEY! Needs to stay empty for correct operation.
      */
-    static auto constexpr rootKeyStr = "";
+    static std::string_view constexpr rootKeyStr = "";
 
     /**
      * @brief The key used to store and retrieve the value being modified.
@@ -61,15 +72,9 @@ public:
     static auto constexpr rootKey = ScopedKeyView(rootKeyStr);
 
 private:
+    static_assert(rootKeyStr.empty(), "The rootKeyStr must be an empty string for correct operation of the transformation module.");
+
     std::shared_ptr<Interaction::Execution::FuncTree<bool, JsonScope*>> transformationFuncTree;
-
-    // Example function for binding
-
-    // NOLINTNEXTLINE
-    static bool bar(Data::JsonScope* scope) {
-        (void)scope;
-        return true;
-    }
 };
 } // namespace Nebulite::Data
 #endif // NEBULITE_DATA_DOCUMENT_TRANSFORMATION_MODULE_HPP
