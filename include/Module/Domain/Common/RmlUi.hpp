99fa1ea2c41c41d315039e9ab9a926da382e62a5
Leo <leo.bastigkeit@gmail.com>
2026-04-30 13:26:02 +0200

    Add header file for RmlUi module with destructor placeholder

---
+++ include/Module/Domain/Common/RmlUi.hpp
@@ -0,0 +1,6 @@
+#ifndef NEBULITE_MODULE_DOMAIN_COMMON_RMLUI_HPP
+#define NEBULITE_MODULE_DOMAIN_COMMON_RMLUI_HPP
+
+// TODO: A module with just a destructor that removes any Renderer RmlUi documents based on the domain id
+
+#endif // NEBULITE_MODULE_DOMAIN_COMMON_RMLUI_HPP
