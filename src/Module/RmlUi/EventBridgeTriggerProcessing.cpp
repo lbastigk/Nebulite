39e9c024bbf9e7569d3948d0892e09e21697a611
Leo <leo.bastigkeit@gmail.com>
2026-05-18 00:00:06 +0200

    Linting

--- src/Module/RmlUi/EventBridgeTriggerProcessing.cpp
+++ src/Module/RmlUi/EventBridgeTriggerProcessing.cpp
@@ -40,7 +40,7 @@ void EventBridge::Attribute::OnEnter::processTrigger(Graphics::RmlInterface& man
     entry.apply(manager, capture);
 }
 
-void EventBridge::Attribute::OnClick::processTrigger(Graphics::RmlInterface& manager, Utility::IO::Capture& capture, SDL_Event const& event, int keyModifiers, Rml::Element* focusElement){
+void EventBridge::Attribute::OnClick::processTrigger(Graphics::RmlInterface& manager, Utility::IO::Capture& capture, SDL_Event const& event, int /*keyModifiers*/, Rml::Element* focusElement){
     if (!focusElement) return;
     if (event.type != SDL_EVENT_MOUSE_BUTTON_DOWN) return;
     BridgeEntry<OnClick> const entry(focusElement);
