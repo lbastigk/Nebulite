96445b357e927774889b0e82fe80bb3144bb089e
Leo <leo.bastigkeit@gmail.com>
2026-06-17 11:00:57 +0200

    Add info and potential improvements to TaskQueueResult

--- include/Data/TaskQueue.hpp
+++ include/Data/TaskQueue.hpp
@@ -50,6 +50,10 @@ public:
      * @brief Represents the result of resolving a task queue.
      *        This structure holds the outcome of processing a task queue, including any errors
      *        encountered during resolution and whether the process was halted due to a critical error.
+     *        Does not include successful results
+     * @note Since Events just hold the type (Warning, Error), returning a vector of results might be unnecessary.
+     *       Although it's sort of useful, as we can count the amount of events and inform: "Task encountered X warnings and Y errors"
+     *       Later on we might change that to 2 size_t that count the amount of warnings and errors, instead of returning a vector of events.
      */
     struct TaskQueueResult {
         bool encounteredCriticalResult = false; // Indicates if a critical error was encountered during task resolution
