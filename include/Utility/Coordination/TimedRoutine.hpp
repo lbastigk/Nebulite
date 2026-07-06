1e33891287965f4c881bf02f57058f04a431aca6
Leo <leo.bastigkeit@gmail.com>
2026-07-06 19:30:45 +0200

    Remove old note

--- include/Utility/Coordination/TimedRoutine.hpp
+++ include/Utility/Coordination/TimedRoutine.hpp
@@ -21,11 +21,6 @@ namespace Nebulite::Utility::Coordination {
 /**
  * @class TimedRoutine
  * @brief A class that schedules a routine to be executed at specified time intervals.
- * @note IDEA: Allow for leisure timing so that a potentially large amount of routines isn't executed all at once.
- *       additional parameter for ExecutionMode: STRICT, LEISURE, using some kind of RNG to spread out execution over multiple updates.
- *       The LEISURE mode should only be used for non-state-modifying routines, as it introduces non-determinism in execution timing.
- *       E.g. for background tasks like cleanup, texture reloading, etc.
- *       Not a high priority, as there isn't currently a use case for it.
  */
 class TimedRoutine {
 public:
