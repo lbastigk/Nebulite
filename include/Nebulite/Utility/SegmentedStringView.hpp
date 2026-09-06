7b6b959ff0c3801718c83f59394c8d64b1b056d8
Leo <leo.bastigkeit@gmail.com>
2026-09-06 14:24:03 +0200

    Add SegmentedStringView class for handling split strings

---
+++ include/Nebulite/Utility/SegmentedStringView.hpp
@@ -0,0 +1,54 @@
+#ifndef NEBULITE_UTILITY_SEGMENTEDSTRINGVIEW_HPP
+#define NEBULITE_UTILITY_SEGMENTEDSTRINGVIEW_HPP
+
+//------------------------------------------
+// Includes
+
+// Standard library
+#include <cstddef>
+#include <span>
+#include <string_view>
+
+//------------------------------------------
+
+namespace Nebulite::Utility {
+/**
+ * @class SegmentedStringView
+ * @brief Provides basic string functionality for a split string
+ * @todo Requires policy setting if between each point a whitespace is assumed
+ *       This is important for recombination and comparison!
+ *       e.g. ["Hello" "world"] might mean "Helloword" or "Hello world", depending on the policy setting
+ */
+class SegmentedStringView {
+    std::span<std::string_view const> const data;
+
+    std::size_t const charCount; // Pre-computed, as it's used for many comparisons
+
+public:
+    explicit SegmentedStringView(std::span<std::string_view const> sv);
+
+    bool operator==(SegmentedStringView const& other) const;
+    bool operator!=(SegmentedStringView const& other) const;
+
+    bool operator==(std::string_view other) const;
+    bool operator!=(std::string_view other) const;
+
+    [[nodiscard]] std::size_t segmentCount() const;
+    [[nodiscard]] std::size_t charactorCount() const;
+
+    [[nodiscard]] SegmentedStringView subspan(std::size_t index) const ;
+    [[nodiscard]] SegmentedStringView subspan(std::size_t startIndex, std::size_t count) const ;
+
+    [[nodiscard]] auto begin() const {
+        return data.begin();
+    }
+
+    [[nodiscard]] auto end() const {
+        return data.end();
+    }
+
+    // TODO: beginsWith, endsWith, contains
+};
+
+} // namespace Nebulite::Utility
+#endif // NEBULITE_UTILITY_SEGMENTEDSTRINGVIEW_HPP
---
+++ src/Nebulite/Utility/SegmentedStringView.cpp
@@ -0,0 +1,100 @@
+//------------------------------------------
+// Includes
+
+// Standard library
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <span>
+#include <string_view>
+
+// Nebulite
+#include "Nebulite/Utility/SegmentedStringView.hpp"
+
+//------------------------------------------
+
+namespace {
+
+std::size_t countCharacters(std::span<std::string_view const> strings) {
+    return std::ranges::fold_left(
+        strings,
+        std::size_t{0},
+        [](std::size_t const acc, std::string_view const s) {
+            return acc + s.size();
+        }
+    );
+}
+
+} // namespace
+
+namespace Nebulite::Utility {
+
+SegmentedStringView::SegmentedStringView(std::span<std::string_view const> const sv) : data(sv), charCount(countCharacters(sv)) {}
+
+bool SegmentedStringView::operator==(SegmentedStringView const& other) const{
+    if (charCount != other.charCount) {
+        return false;
+    }
+
+    // Range position
+    auto itA = data.begin();
+    auto itB = other.data.begin();
+
+    // Char position in string_view
+    std::size_t posA = 0;
+    std::size_t posB = 0;
+
+    while (itA != data.end() && itB != other.data.end()) {
+        // Check if segment end is reached
+        if (posA == itA->size()) {
+            ++itA;
+            posA = 0;
+            continue;
+        }
+        if (posB == itB->size()) {
+            ++itB;
+            posB = 0;
+            continue;
+        }
+
+        if ((*itA)[posA] != (*itB)[posB]) {
+            return false;
+        }
+        ++posA;
+        ++posB;
+    }
+
+    //return itA == data.end() && itB == other.data.end(); // Since charCount must be equal, this is unnecessary
+    return true;
+}
+
+bool SegmentedStringView::operator!=(SegmentedStringView const& other) const{
+    return !(*this == other);
+}
+
+// array init likely unnecessary, direct comparison would be faster
+bool SegmentedStringView::operator==(std::string_view const other) const {
+    std::array const otherData = {other};
+    return *this == SegmentedStringView{std::span<std::string_view const>{otherData}};
+}
+
+bool SegmentedStringView::operator!=(std::string_view const other) const{
+    return !(*this == other);
+}
+
+std::size_t SegmentedStringView::charactorCount() const {
+    return charCount;
+}
+
+std::size_t SegmentedStringView::segmentCount() const {
+    return data.size();
+}
+
+SegmentedStringView SegmentedStringView::subspan(std::size_t const index) const {
+    return SegmentedStringView(data.subspan(index));
+}
+SegmentedStringView SegmentedStringView::subspan(std::size_t const startIndex, std::size_t const count) const {
+    return SegmentedStringView(data.subspan(startIndex, count));
+}
+
+} // namespace Nebulite::Utility
