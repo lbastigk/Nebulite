021c806303ce5602e631a5584f2cddd02b18b1e7
Leo <leo.bastigkeit@gmail.com>
2026-09-06 20:00:00 +0200

    Fix getRenderOffset of Circle DrawType

--- src/Nebulite/Graphics/DrawType/Circle.cpp
+++ src/Nebulite/Graphics/DrawType/Circle.cpp
@@ -64,8 +64,8 @@ bool Circle::diff(Data::JsonScope& /*scope*/, DrawcallRefs& refs) {
 }
 Math::Vec2<float> Circle::getRenderOffset(DrawcallRefs& refs) {
     return Math::Vec2<float>{
-        .x = static_cast<float>(*refs.rectDstW / 2.0),
-        .y = static_cast<float>(*refs.rectDstH / 2.0),
+        .x = - static_cast<float>(*refs.rectDstW / 2.0),
+        .y = - static_cast<float>(*refs.rectDstH / 2.0),
     };
 }
 
